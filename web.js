ce3bf84360c6790eafcb792e157a0f707921607f
James Duncan <japaclodun@gmail.com>
2013-08-13 19:26:52 +0000

    Updated to read and send index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var index = fs.readFileSync('./index.html', 'utf-8');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
